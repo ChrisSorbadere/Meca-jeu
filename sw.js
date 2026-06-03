fa7a91b3baf462290ba2bb7b812ef247c9168fe8
ChrisSorbadere <chrissorbadere@gmail.com>
2026-06-03 10:35:18 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,28 @@
+// Service worker : met en cache les fichiers pour le hors-ligne et l'installation PWA.
+const CACHE='meca-v1';
+const FILES=['./','./index.html','./manifest.json','./icon-192.png','./icon-512.png'];
+
+self.addEventListener('install',e=>{
+  e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate',e=>{
+  e.waitUntil(caches.keys().then(keys=>
+    Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))
+  ));
+  self.clients.claim();
+});
+
+self.addEventListener('fetch',e=>{
+  // stratégie : réseau d'abord, puis cache en secours (hors-ligne)
+  e.respondWith(
+    fetch(e.request)
+      .then(resp=>{
+        const copy=resp.clone();
+        caches.open(CACHE).then(c=>c.put(e.request,copy)).catch(()=>{});
+        return resp;
+      })
+      .catch(()=>caches.match(e.request).then(r=>r||caches.match('./index.html')))
+  );
+});

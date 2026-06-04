9345f0ee9538464870014ae2566e576df61e4f8e
ChrisSorbadere <chrissorbadere@gmail.com>
2026-06-04 05:26:55 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Service worker : met en cache les fichiers pour le hors-ligne et l'installation PWA.
-const CACHE='meca-v16';
+const CACHE='meca-v17';
 const FILES=['./','./index.html','./manifest.json','./icon-192.png','./icon-512.png'];
 
 self.addEventListener('install',e=>{
